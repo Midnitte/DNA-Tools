d4bd39c3c99e17f4571e4aa1879fd5ce9d685914
Midnitte <midnitte@gmail.com>
2022-06-01 13:31:10 -0400

    FInished Amino Acid function, begin Codon function

--- dna-tools.py
+++ dna-tools.py
@@ -8,19 +8,58 @@ def transcription(dna):
     translated = dna.maketrans("ATGC", "UACG")
     return dna.translate(translated)
 
-def amino_acids(dna):
-    """Convert mRNA to amino acids."""
-    match dna:
+# TODO
+def codons(mrna):
+    """Break up mRNA into codons"""
+    return ""
+
+# TODO: decorator that prints return statements when debug on...
+def amino_acids(codon):
+    """Convert mRNA codons to amino acids."""
+    match codon:
         case "AUG":
             return "Methionine"
         case ["AUA", "AUC", "AUU"]:
             return "Isoleucine"
         case ["ACG", "ACA", "ACC", "ACU"]:
             return "Threonine"
-        case ["AGG", "AGA"]: #Add more Arginine and Serine matches
+        case ["AGG", "AGA", "CGG", "CGA", "CGC", "CGU"]:
             return "Arginine"
-        case ["AGC", "AGU"]:
+        case ["AGC", "AGU", "UCG", "UCA", "UCC", "UCU"]:
             return "Serine"
+        case ["AAG", "AAA"]:
+            return "Lysine"
+        case ["AAC", "AAU"]:
+            return "Asparagine"
+        case ["CUG", "CUA", "CUC", "CUU", "UUG", "UUA"]:
+            return "Leucine"
+        case ["CCG", "CCA", "CCC", "CCU"]:
+            return "Proline"
+        case ["CAG", "CAA"]:
+            return "Glutamine"
+        case ["CAC", "CAU"]:
+            return "Histidine"
+        case ["UUC", "UUU"]:
+            return "Phenylalanine"
+        case ["UGG"]:
+            return "Tryptophan"
+        case ["UGC", "UGU"]:
+            return "Cysteine"
+        case ["UAC", "UAU"]:
+            return "Tyrosine"
+        case ["GUG", "GUA", "GUC", "GUU"]:
+            return "Valine"
+        case ["GCG", "GCA", "GCC", "GCU"]:
+            return "Alanine"
+        case ["GGG", "GGA", "GGC", "GGU"]:
+            return "Glycine"
+        case ["GAG", "GAA"]:
+            return "Glutamic Acid"
+        case ["GAC", "GAU"]:
+            return "Aspartic Acid"
+        case ["UGA", "UAG", "UAA"]:
+            return "Stop" # Return stop, or "pass"?
+
         # WIP
         
 
